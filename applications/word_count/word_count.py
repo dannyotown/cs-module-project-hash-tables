46b8780379ecea08ceeea0e48b4849639fe89193
Danny Town <47186976+dannyotown@users.noreply.github.com>
2020-08-11 21:15:17 -0400

    word count almost done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,19 @@
 def word_count(s):
     # Your code here
-
+    d = {}
+    check_set = {',',':',';','\'','"','.','-', '+', '=', '/', '\r', '|', '[' ,']', '{', '}', '(', ')', '*', '^', '&'}
+    if s == '':
+        return d
+    split_s = s.lower().split(' ')
+    for i in range(len(split_s)):
+        test = [i.split('\t',1)[0] for i in split_s[i] if i not in check_set]
+        new_str = ''.join(test)
+        if new_str in d:
+            d[new_str] += 1
+        else:
+            d[new_str] = 1
+    return d
+        
 
 
 if __name__ == "__main__":
