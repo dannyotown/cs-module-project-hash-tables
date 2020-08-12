5de5d8236f1538f59130523ccb104a7009e9a140
Danny Town <47186976+dannyotown@users.noreply.github.com>
2020-08-12 10:18:04 -0400

    markov somewhat complete/word count still has issues

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,32 @@
-import random
-
+from random import randrange
+d = {}
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
-
-# TODO: analyze which words can follow other words
-# Your code here
+    split_words = words.split(' ')
+    for i in range(len(split_words)-1):
+        if split_words[i] in d:
+            d[split_words[i]].append(split_words[i+1])
+        else:
+            d[split_words[i]] = [split_words[i+1]]
 
 
 # TODO: construct 5 random sentences
 # Your code here
+def construct_random(s):
+    new = s.split(' ')
+    for i in range(len(new)):
+        if new[i] in d:
+            new[i] = d[new[i]][randrange(len(d[new[i]]))]
+    new_str = ' '.join(new[:-1]).replace('\n', ' ')
+    print(new_str)
 
+
+construct_random('What, you were thirsty, were you?')
+construct_random(
+    'Now for number three: you unwound every bit of the worsted while I wasn\'t looking!')
+construct_random(
+    'That\'s three faults, Kitty, and you\'ve not been punished for any of them yet.')
+construct_random(
+    'Kitty, can you play chess?')
+construct_random('And I do so wish it was true!')
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,23 +1,15 @@
+import re
+
+
 def word_count(s):
     # Your code here
-    d = {}
-    check_set = {',',':',';','\'','"','.','-', '+', '=', '/', '\r', '|', '[' ,']', '{', '}', '(', ')', '*', '^', '&'}
-    if s == '':
-        return d
-    split_s = s.lower().split(' ')
-    for i in range(len(split_s)):
-        test = [i.split('\t',1)[0] for i in split_s[i] if i not in check_set]
-        new_str = ''.join(test)
-        if new_str in d:
-            d[new_str] += 1
-        else:
-            d[new_str] = 1
-    return d
-        
+    print(re.sub('":;,.-+=/\|[]{}()*^&', '', '":;,.-+=/\|[]{}()*^&'))
 
 
 if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    # print(word_count(""))
+    # print(word_count("Hello"))
+    # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    # print(word_count(
+    #     'This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
